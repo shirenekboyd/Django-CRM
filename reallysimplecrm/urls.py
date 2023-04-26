30e4c292451bf6b0051f8e4cb7d48fd496335254
Shirene Boyd <shirenekadkhodai@gmail.com>
2023-04-26 11:54:05 -0500

    updated home.html with bootstrap form code and customized it to meet my specifications for users to be able to login on the home page

--- reallysimplecrm/urls.py
+++ reallysimplecrm/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
    
    # home page
-   path('', views.home, name='home')
+   path('', views.home, name='home'),
+#    path('login/', views.login_user, name='login'),
+#    path('logout/', views.logout_user, name='logout'),
+   
 ]
--- reallysimplecrm/views.py
+++ reallysimplecrm/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages 
 
 # Create your views here.
+
+# Notes
+# request = someone going into a web page and requesting to do something like login or logout
+# login and logout are functions that's why we must use def login_user and def logout_user
+
+
 
 # Home page
 def home(request):
     return render(request, 'home.html', {})
+
+# Login/Logout User 
+# def login_user(request):
+#     pass
+
+def logout_user(request):
+    pass 
