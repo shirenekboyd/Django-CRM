0741135f6851e7d64190b41d90e54bc173ed1057
Shirene Boyd <shirenekadkhodai@gmail.com>
2023-04-27 12:49:23 -0500

    created a Contact model in models.py file, registered my model in admin.py file, and ran database migrations

--- reallysimplecrm/models.py
+++ reallysimplecrm/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+# Django code/Python that creates a class that will then abstract away and create that code for us on the backend which will create the SQLite3 code behind the scenes
+
+class Contact(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(blank=True, null=True)
+    address_line_1 = models.CharField(max_length=100, blank=True, null=True)
+    address_line_2 = models.CharField(max_length=100, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    state = models.CharField(max_length=100, blank=True, null=True)
+    zipcode = models.CharField(max_length=10, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
